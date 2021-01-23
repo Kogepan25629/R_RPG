e43e837c85d5e8928503416d4ab1f242b58e580b
Kogepan25629 <1129ryou1129@gmail.com@>
2021-01-23 23:53:01 +0900

    ウィンドウサイズを変更できるように

--- R_RPG/General_Data.cs
+++ R_RPG/General_Data.cs
@@ -8,6 +8,18 @@ namespace R_RPG
 {
     static class GeD // GeneralData
     {
-        static public int Window_Width, Window_Heigt;   // ウィンドウサイズ
+        // 定数
+        public static readonly int BUTTON1_SIZE1X;
+        public static readonly int BUTTON1_SIZE1Y;
+        public static readonly int BUTTON1_SIZE2X;
+        public static readonly int BUTTON1_SIZE2Y;
+        public static readonly int BUTTON1_SIZE3X;
+        public static readonly int BUTTON1_SIZE3Y;
+        public static readonly int BUTTON1_SIZE4X;
+        public static readonly int BUTTON1_SIZE4Y;
+
+
+        // 変数
+        public static int Window_Width, Window_Heigt;   // ウィンドウサイズ
     }
 }
