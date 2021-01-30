bdfb91943ed7e0cc7dd4a8cc606b458f51bda599
Kogepan25629 <1129ryou1129@gmail.com>
2021-01-30 23:40:01 +0900

    コードのクリーンアップ

--- R_RPG/Game_Scene/Control/C_GameEscMenu.cs
+++ R_RPG/Game_Scene/Control/C_GameEscMenu.cs
@@ -18,29 +18,30 @@ namespace R_RPG.Game_Scene
         // このクラスの初期化
         private static void CGameEscMenuInit()
         {
+            // 変数の宣言と値の計算
             int ButtonWidth1 = (int)(GeD.Window_Width * 0.2);
             int ButtonHeight1 = (int)(GeD.Window_Heigt * 0.1);
             int DrawSpaceX = (int)(GeD.Window_Width * 0.05);
             int DrawSpaceY = (int)(GeD.Window_Heigt * 0.05);
             int ButtonPositionX = GeD.Window_Width / 2 - DrawSpaceX / 2 - ButtonWidth1;
             int ButtonPositionY = (int)(GeD.Window_Heigt * 0.2 + ButtonHeight1);
-
+            // ボタンのインスタンスを生成
             Button1 = new RUI_Button();
             Button2 = new RUI_Button();
             Button3 = new RUI_Button();
             Button4 = new RUI_Button();
-
+            // Button1
             Button1.SetPoint(ButtonPositionX, ButtonPositionY, ButtonPositionX + ButtonWidth1, ButtonPositionY + ButtonHeight1);
             Button1.Mode = 3;
             Button1.SetString("設定", FoD.TestFont);
             Button1.GrHandle = Graphic_Data.GraphicData["Black"];
-
+            // Button2
             ButtonPositionX = ButtonPositionX + DrawSpaceX + ButtonWidth1;
             Button2.SetPoint(ButtonPositionX, ButtonPositionY, ButtonPositionX + ButtonWidth1, ButtonPositionY + ButtonHeight1);
             Button2.Mode = 3;
             Button2.SetString("実績", FoD.TestFont);
             Button2.GrHandle = Graphic_Data.GraphicData["Black"];
-
+            // Button3
             ButtonPositionX = ButtonPositionX - DrawSpaceX - ButtonWidth1;
             ButtonPositionY += ButtonHeight1 + DrawSpaceY;
             ButtonWidth1 += ButtonWidth1 + DrawSpaceX;
@@ -59,10 +60,10 @@ namespace R_RPG.Game_Scene
                 EnableInit = false;
             }
 
-            //Draw_Esc_Menu
+            // Draw_Esc_Menu
             int ButtonResult = Draw_Esc_Menu();
 
-            //Escを押した時
+            // Escを押した時
             if((GeD.KeyState[DX.KEY_INPUT_ESCAPE] == 1 && GeD.KeyStateOld[DX.KEY_INPUT_ESCAPE] == 0) || ButtonResult == 1)
             {
                 DX.SetDrawBright(255, 255, 255);    // 通常の明るさにする(明るさを戻す)
@@ -73,7 +74,7 @@ namespace R_RPG.Game_Scene
             return 0;// 一時的
         }
 
-        //Draw_Esc_Menu
+        // Draw_Esc_Menu
         private static byte Draw_Esc_Menu()
         {
             try
@@ -85,7 +86,7 @@ namespace R_RPG.Game_Scene
                 Button2.Show();
                 Button3.Show();
 
-                //ボタンのクリック判定
+                // ボタンのクリック判定
                 if (Button1.LeftUpDetection() == true)
                 {
                     return 2;
