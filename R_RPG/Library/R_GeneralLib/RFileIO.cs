f6f621afcae6f6fadd2546879486ccefdc37779d
Kogepan25629 <1129ryou1129@gmail.com>
2021-03-07 01:07:11 +0900

    R_GLibの更新

---
+++ R_RPG/Library/R_GeneralLib/RFileIO.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace R_GeneralLib
+{
+    class RFileIO
+    {
+        //// 変数
+        //private static bool OutputSystemLog;
+        private string FilePath;
+        private Encoding Encoding;
+        private char SplitChar = '=';
+        private Dictionary<string, string> DictionaryData;
+
+        // コンストラクタ
+        public RFileIO(string filePath, string encoding, char splitChar = '=')
+        {
+            // エンコードの文字コードを指定
+            try {
+                Encoding = Encoding.GetEncoding(encoding);
+            }
+            catch (ArgumentException) {
+                RG.OutputLog("[R_GeneralLib.RFileIO][コンストラクタ] encordingの値[" + encoding + "]は正しくありません。UTF-8を使用します。", RG.ERROR);
+                Encoding = Encoding.GetEncoding("UTF-8");
+            }
+            // FilePathの変更
+            FilePath = filePath;
+            // ファイルで
+            SplitChar = splitChar;
+        }
+
+        // CreateFile
+        public int CreateFile()
+        {
+            if (File.Exists(FilePath) == false) {
+                FileStream fileOptionsCreate = File.Create(FilePath);
+                fileOptionsCreate.Close();
+                RG.OutputLog("[R_GeneralLib.RFileIO][関数:CreateFile] " + FilePath + " が生成されました", RG.INFO);
+                return 0;
+            }
+            else {
+                RG.OutputLog("[R_GeneralLib.RFileIO][関数:CreateFile] " + FilePath + " はすでに存在しています", RG.WARM);
+                return 0;
+            }
+        }
+
+        // LoadFile
+        public int LoadFile()
+        {
+            // ファイルが存在しなければ規定値で生成
+            if (File.Exists(FilePath) == false) {
+                CreateFile();
+            }
+
+            // ファイルを開く
+            FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader file = new StreamReader(fs, Encoding);
+
+            // 読み込み
+            while (file.Peek() != -1) {
+                // 1行ずつ読み込み
+                string str = file.ReadLine();
+
+                // SplitChar でくぎる
+                char[] charSeparator = { SplitChar };
+                string[] arr = str.Split(charSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+                // 要素数が2のとき(Key, Value)読み込んだ値を対応するDictionaryDataに格納
+                if (arr.Length == 2) {
+                    //Console.WriteLine(arr[0] + ":" + arr[1]);
+
+                    bool existkey = false;
+                    // 対応するKeyを探す
+                    foreach (KeyValuePair<string, string> item in DictionaryData) {
+                        if (item.Key == arr[0]) {
+                            existkey = true;
+                        }
+                    }
+                    if (existkey == true) {
+                        SetValue(arr[0], arr[1]);
+                    }
+                    else {
+                        DictionaryData.Add(arr[0], arr[1]);
+                    }
+                }
+            }
+
+            // ファイルを閉じる
+            file.Close();
+            fs.Close();
+            RG.OutputLog("[R_GeneralLib.RFileIO][関数:LoadFile] " + FilePath + " から読み込みました", RG.INFO);
+            return 0;
+
+        }
+
+        // SaveFile
+        public int SaveFile()
+        {
+            if (File.Exists(FilePath) == false) {
+                CreateFile();
+            }
+
+            try {
+                // ファイルを開く
+                StreamWriter fileOptions = new StreamWriter(FilePath, false, Encoding);
+
+                // テキストを書き込む
+                foreach (KeyValuePair<string, string> item in DictionaryData) {
+                    fileOptions.WriteLine(item.Key + SplitChar + item.Value);
+                }
+
+                // ファイルを閉じる
+                fileOptions.Close();
+                RG.OutputLog("[R_GeneralLib.RFileIO][関数:SaveFile] " + FilePath + " への書き込みが正常に行われました", RG.INFO);
+                return 0;
+            }
+            catch (System.IO.IOException) {
+                RG.OutputLog("[R_GeneralLib.RFileIO][関数:SaveFile] " + FilePath + " ファイルにアクセスできません。別のプロセスが使用中です", RG.ERROR);
+                RG.OutputLog("[R_GeneralLib.RFileIO][関数:SaveFile] " + FilePath + " への書き込みがスルーされました。", RG.ERROR);
+                return -1;
+            }
+        }
+
+        // キーが存在するかどうか確認
+        public bool CheckKey(string key)
+        {
+            string tmp;
+            try {
+                tmp = DictionaryData[key];
+                return true;
+            }
+            catch (System.Collections.Generic.KeyNotFoundException) {
+                return false;
+            }
+        }
+
+        // データを追加
+        public void AddValue(string Key, string Value)
+        {
+            DictionaryData.Add(Key, Value);
+        }
+
+        // キーに対する値を設定
+        public int SetValue(string key, string value)
+        {
+            try {
+                DictionaryData[key] = value;
+                return 0;
+            }
+            catch (System.Collections.Generic.KeyNotFoundException) {
+                RG.OutputLog("[R_GeneralLib.RFileData][関数:SetValue] key[" + key + "]は存在しません", RG.ERROR);
+                return -1;
+            }
+        }
+
+        // キーに対する値を取得
+        public string GetValue(string key)
+        {
+            try {
+                return DictionaryData[key];
+            }
+            catch (System.Collections.Generic.KeyNotFoundException) {
+                RG.OutputLog("[R_GeneralLib.RFileData][関数:GetValue] key[" + key + "]は存在しません", RG.ERROR);
+                return default;
+            }
+        }
+
+        // キーに対する値を任意の型で取得
+        public Types GetValue<Types>(string key)
+        {
+            try {
+                return RG.ConvertTypes<Types>(DictionaryData[key].ToString());
+            }
+            catch (System.Collections.Generic.KeyNotFoundException) {
+                RG.OutputLog("[R_GeneralLib.RFileData][関数:GetValue] key[" + key + "]は存在しません" + "型:" + typeof(Types).ToString(), RG.ERROR);
+                return default;
+            }
+        }
+
+        // キーに対する値をまとめてDictionaryで設定
+        public void SetAllValue(Dictionary<string, string> dictionaryData)
+        {
+            DictionaryData = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string> item in dictionaryData) {
+                DictionaryData[item.Key] = item.Value;
+            }
+        }
+        public void SetAllValue(Dictionary<string, object> dictionaryData)
+        {
+            DictionaryData = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, object> item in dictionaryData) {
+                DictionaryData[item.Key] = item.Value.ToString();
+            }
+        }
+
+        // キーに対する値をまとめてDictionaryで取得
+        public Dictionary<string, string> GetAllValue()
+        {
+            Dictionary<string, string> dictionaryData = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string> item in DictionaryData) {
+                dictionaryData[item.Key] = item.Value;
+            }
+            return dictionaryData;
+        }
+    }
+}
---
+++ R_RPG/Library/R_GeneralLib/RG.cs
@@ -0,0 +1,122 @@
+﻿//Version 1.3.4
+
+using System;
+
+namespace R_GeneralLib
+{
+    public static class RG
+    {
+        // 定数
+        public const byte INFO = 0;
+        public const byte WARM = 1;
+        public const byte ERROR = 2;
+        public const byte NONE = 3;
+
+        //変数
+        private static byte OutputLogLevel = INFO;
+
+        // コンソール出力のレベル変更(ERROR, WARM, NORMAL, NONE)
+        public static void ChangeOutputLogLevel(byte outputLogLevel)
+        {
+            if (outputLogLevel == INFO || outputLogLevel == WARM || outputLogLevel == ERROR || outputLogLevel == NONE) {
+                OutputLogLevel = outputLogLevel;
+            }
+        }
+
+        // コンソール出力関数
+        public static void OutputLog(string str, byte outputLogLevel = INFO, bool enableTime = true)
+        {
+            if (outputLogLevel >= OutputLogLevel) {
+                //時間表示
+                if (enableTime == true) {
+                    DateTime dt = DateTime.Now;
+
+                    // 各桁が1桁の場合先頭に0をつける
+                    string hour = dt.Hour.ToString();
+                    string minute = dt.Minute.ToString();
+                    string second = dt.Second.ToString();
+                    if (hour.Length == 1) {
+                        hour = "0" + hour;
+                    }else if(minute.Length == 1) {
+                        minute = "0" + minute;
+                    }
+                    else if(second.Length == 1) {
+                        second = "0" + second;
+                    }
+
+                    Console.Write("[" + hour + ":" + minute + ":" + second + "]");
+                }
+
+                // modeに応じて文字列の色を変更
+                if (outputLogLevel == WARM) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;  // WARMは黄色
+                }
+                else if (outputLogLevel == ERROR) {
+                    Console.ForegroundColor = ConsoleColor.Red;     // ERRORは赤色
+                }
+
+                // 時間の後に空白
+                if (enableTime == true) {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(str); // 文字列を指定された色で出力
+                Console.ResetColor();   // 色をリセット
+            }
+        }
+
+        // 文字列を指定した任意の型で返す
+        public static Types ConvertTypes<Types>(string str)
+        {
+            bool failedParse = false;
+            if (typeof(Types) == typeof(string)) {  // string
+                return (Types)(object)str;
+            }
+            else if (typeof(Types) == typeof(int)) {  // int
+                int intNum;
+                if (int.TryParse(str, out intNum)) {
+                    return (Types)(object)intNum;
+                }
+                else {
+                    failedParse = true;
+                }
+            }
+            else if (typeof(Types) == typeof(double)) {  // double
+                double doubleNum;
+                if (double.TryParse(str, out doubleNum)) {
+                    return (Types)(object)doubleNum;
+                }
+                else {
+                    failedParse = true;
+                }
+            }
+            else if (typeof(Types) == typeof(float)) {  // float
+                float floatNum;
+                if (float.TryParse(str, out floatNum)) {
+                    return (Types)(object)floatNum;
+                }
+                else {
+                    failedParse = true;
+                }
+            }
+            else if (typeof(Types) == typeof(bool)) {  // bool
+                bool boolNum;
+                if (bool.TryParse(str, out boolNum)) {
+                    return (Types)(object)boolNum;
+                }
+                else {
+                    failedParse = true;
+                }
+            }
+
+            // 正常に型変換されなかった場合
+            if (failedParse == true) {
+                RG.OutputLog("[R_GeneralLib.RG][関数:GetValue] 型変換に失敗" + "[値:" + str + "; 型:" + typeof(Types).ToString() + "]", RG.ERROR);
+            }
+            else {
+                RG.OutputLog("[R_GeneralLib.RG][関数:GetValue] 型[" + typeof(Types).ToString() + "]に対応していません", RG.ERROR);
+            }
+            return default;
+        }
+    }
+}
